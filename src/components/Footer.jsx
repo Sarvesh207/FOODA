1b4c966bfd5e428e32f9181000aa8698cc2e07e4
Sarvesh207 <sarveshgaynar@gmail.com>
2024-01-27 13:23:45 +0530

    footer changes done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -55,35 +55,19 @@ const Footer = () => {
                         </div>
                     ))}
                 </div>
-                <div className="flex text-gray-100 w-full flex-col items-center justify-center border-t border-blue-gray-50 py-4 md:flex-row md:justify-between">
+                <div className="flex text-center text-gray-100 w-full flex-col items-center justify-center border-t border-blue-gray-50 py-4 md:flex-row md:justify-between">
                     <Typography
                         variant="small"
                         className="mb-4 text-center font-normal text-blue-gray-900 md:mb-0"
                     >
-                        &copy; {currentYear} <a href="">BiteMates @</a>. All
+                        &copy; {currentYear} <a href="/">FOODA @</a>. All
                         Rights Reserved. @sarveshgaynar
                     </Typography>
                     <div className="flex gap-4 text-blue-gray-900 sm:justify-center">
+                        
                         <Typography
                             as="a"
-                            href="#"
-                            className="opacity-80 transition-opacity hover:opacity-100"
-                        >
-                            <svg
-                                className="h-5 w-5"
-                                fill="currentColor"
-                                viewBox="0 0 24 24"
-                                aria-hidden="true"
-                            >
-                                <path
-                                    fill-rule="evenodd"
-                                    d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
-                                    clip-rule="evenodd"
-                                />
-                            </svg>
-                        </Typography>
-                        <Typography
-                            as="a"
+                            target="_blank" rel="noopener noreferrer"
                             href="https://www.instagram.com/sarvesh207_/"
                             className="opacity-80 transition-opacity hover:opacity-100"
                         >
@@ -102,7 +86,8 @@ const Footer = () => {
                         </Typography>
                         <Typography
                             as="a"
-                            href="https://twitter.com/Sarvesh_207"
+                            target="_blank" rel="noopener noreferrer"
+                            href="https://twitter.com/gaynarsarvesh"
                             className="opacity-80 transition-opacity hover:opacity-100"
                         >
                             <svg
@@ -116,6 +101,7 @@ const Footer = () => {
                         </Typography>
                         <Typography
                             as="a"
+                            target="_blank" rel="noopener noreferrer"
                             href="http://github.com/Sarvesh207"
                             className="opacity-80 transition-opacity hover:opacity-100"
                         >
