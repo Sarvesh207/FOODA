de683812898461b68dd56eb68530dfdbf9819164
Sarvesh207 <sarveshgaynar@gmail.com>
2024-10-21 23:48:43 +0530

    change background of signup and login

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -3,7 +3,7 @@ import UserForm from '../components/Login/UserForm'
 
 function SignUp() {
   return (
-    <div className='full-background-singUp'>
+    <div className='full-background-singUp  '>
     <UserForm formType="signup"/>
     </div>
   )
